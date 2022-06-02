bba49ae8b6fd59e544c8aa6fec559bec56cdf50a
Rose <odhiamborose466@gmail.com>
2022-06-02 16:03:47 +0300

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 console.log('hello');
 const express = require('express');
 const dotenv = require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3555;
 const { errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
